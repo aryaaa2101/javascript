9d704e30414bab9da3f0c56385b1f850e29b243c
arya <76617993+aryaaa2101@users.noreply.github.com>
2024-04-23 09:07:17 +0000

    datatype_conversion

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,16 @@
+let score = "33"
+let score1 = "33abc"
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+let new_value = Number(score1)
+
+console.log(new_value)
+
+//Nan => not a number
+
+//we can convert into boolean
+// true-1,false-0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
