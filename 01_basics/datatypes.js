3208284419906220413a250c92d1641e67fc2eda
arya <76617993+aryaaa2101@users.noreply.github.com>
2024-04-23 08:29:11 +0000

    data_types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treats all JS codes as newer version
+// alert (3+3), this will give error because we are using node and not browser
+// node is a software used to run JS codes, earlier(now also) we used browser to run.
+
+let name = "Arya"
+let age = 4
+let loggedIn = true
+
+// data types:
+//number, range is => 2 to power 53
+//bigint
+//string
+//boolean
+//null =>standalone value
+//undefined
+//symbol: to find uniqueness
+//object 
+
+console.log(typeof "Arya");
+console.log(typeof null);
