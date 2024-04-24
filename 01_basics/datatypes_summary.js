1122aad42b2070b7e96af87829cf333080ba024c
arya <76617993+aryaaa2101@users.noreply.github.com>
2024-04-24 11:37:30 +0000

    understood stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -14,31 +14,57 @@
   
 //Javascript is dynamically typed language
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id==anotherId);
-//this results in false because symbol looks for uniqueness
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id==anotherId);
+// //this results in false because symbol looks for uniqueness
 
-const bigNumber = 23456789345n
-
-
-
-//Array,objects,functions
-const heros = ["shaktiman", "nagraj","doga"];
-let myObj = {
-    name:"Arya",
-    age :11,
-}
-
-const myFunction = function(){
-    console.log("Hello world");
-}
+// const bigNumber = 23456789345n
 
 
-console.log(typeof id);
-console.log(typeof bigNumber)
-console.log(typeof heros)
-console.log(typeof myFunction)
+
+// //Array,objects,functions
+// const heros = ["shaktiman", "nagraj","doga"];
+// let myObj = {
+//     name:"Arya",
+//     age :11,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+
+// console.log(typeof id);
+// console.log(typeof bigNumber)
+// console.log(typeof heros)
+// console.log(typeof myFunction)
 
 
 // refer https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//There are two types of memory management in JS
+//1. Stack(Primitve data types)
+//2. Heap(Reference or non-primitive data types)
+
+
+// In stack whenever we declare any variable we get its copy
+//In heap whenever anything is defined we get it's reference
+
+let myYoutubename = "aryashukla"
+let anothername = myYoutubename
+anothername = "amishashukla"
+console.log(anothername);
+console.log(myYoutubename)
+
+let userOne ={
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+userTwo.email = "arya@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
