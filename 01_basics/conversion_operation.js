aa61d4afbdfc4a492296b105c78e8f6c1a068aac
arya <76617993+aryaaa2101@users.noreply.github.com>
2024-04-24 05:27:32 +0000

    conversion

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,19 @@
+let value = 8
+let neg = -value
+console.log(neg)
+
+
+let str1 = "Hello"
+let str2 = " Arya"
+let str3 = str1+str2
+console.log(str3)
+
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1"+"2")
+console.log("1"+ 2+ 3)
+console.log(1+2+"3")
+//for the above codes refer ecma script
+
+
