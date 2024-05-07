0e07d71931e0555ed5dbf6350affe53df188a4ed
arya <76617993+aryaaa2101@users.noreply.github.com>
2024-05-07 11:33:53 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,13 @@
+function sayMyName(){
+    console.log("A");
+    console.log("R");
+    console.log("Y");
+    console.log("A");
+}
+//sayMyName()
+function addTwoNumbers(numberOne,numberTwo){
+    const res = numberOne+numberTwo
+    console.log(res)
+
+}
+addTwoNumbers(4,5)
