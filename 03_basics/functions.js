093c83192136ac3bf62a002a25766f4020c80ac5
arya <76617993+aryaaa2101@users.noreply.github.com>
2024-05-13 05:24:08 +0000

    functions

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -5,9 +5,21 @@ function sayMyName(){
     console.log("A");
 }
 //sayMyName()
-function addTwoNumbers(numberOne,numberTwo){
-    const res = numberOne+numberTwo
-    console.log(res)
+function addTwoNumbers(numberOne,numberTwo) { //the variable in the function definition inside paranthesis is called parameters
+    console.log(numberOne + numberTwo)
+}
+addTwoNumbers(3,4)//and here the values that are being passed are known as arguments
+
+function logInUsername(username){
+    if(username === undefined){
+        console.log("Please enter a username")
+        return //this return will not let the next return to execute
+    }
+    return `${username} just logged in`
 
 }
-addTwoNumbers(4,5)
+console.log(logInUsername("Arya"))
+
+//now if we don't pass any value above it results in undefined
+//so in order to eliminate this undefined we use if condition
+
