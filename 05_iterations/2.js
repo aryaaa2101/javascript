a1585324cefb73d70c00614d383b152d03ab7a30
arya <76617993+aryaaa2101@users.noreply.github.com>
2024-05-22 10:38:56 +0000

    while do while

---
+++ 05_iterations/2.js
@@ -0,0 +1,21 @@
+let i = 0
+while (i<=10) {
+    console.log(`Value of i is ${i}`);
+    i = i+2
+    
+}
+
+let myArray = ["Arya","Shami","Shivesh"]
+let arr = 0
+while(arr<myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr  = arr+1
+}
+
+
+ let score = 9
+do {
+    console.log(`Score is ${score}`);
+    score++;
+    
+} while (score<=10);
